59d7b0a1ec32c89484dfdd4f30264884f2c80127
Ramon Schmidt Rocha <ramonroc@gmail.com>
2019-08-06 20:56:08 -0300

    Exercise async await and chapter added

---
+++ chapters/async-await.md
@@ -0,0 +1,3 @@
+#ASYNC - AWAIT
+
+This syntax came in ES2017, to be a good solution for callback hells and chaining responses.
---
+++ exercises/ex3-async-await.js
@@ -0,0 +1,34 @@
+function getFile(file) {
+  return new Promise(function(resolve) {
+    fakeAjax(file, resolve);
+  });
+}
+
+/**
+ * Load files asynchronously and print them synchronously
+ * The await pr must be inside a regular for loop because if it was inside a function
+ * it will cause an syntax error, saying that the word await can not be inside a function
+ */
+async function loadFiles(files) {
+  var prs = files.map(getFile);
+  for (let pr of prs) {
+    console.log(await pr);
+  }
+}
+
+loadFiles(["file1", "file2", "file3"]);
+
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    file1: "the first text",
+    file2: "the second text",
+    file3: "the third text"
+  };
+  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
+
+  console.log("Requesting: " + url);
+
+  setTimeout(function() {
+    cb(fake_responses[url]);
+  }, randomDelay);
+}
